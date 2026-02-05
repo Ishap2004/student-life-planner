0c7b175d01cb196cd6e4d3867737e1bc38baba5b
ishaR0809 <ishaspatel089@gmail.com>
2026-02-05 12:45:43 -0500

    Update server port for deployment

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,20 @@ app.get("/", (req, res) => {
   res.send("Student Life Planner API is running");
 });
 
-// Database connection (will fix later)
+// Database connection
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("DB Error:", err.message));
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("DB Error:", err.message);
+  });
+
+// Port for local + deployment
+const PORT = process.env.PORT || 5000;
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
